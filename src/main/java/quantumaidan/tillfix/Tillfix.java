34c6f56ed1c2d8812ea83377654f21696d5dce97
Aidan <quantumaidan@gmail.com>
2024-11-04 15:42:17 -0600

    actually made the mod work lol 1.20.2 i believe at the moment

--- src/main/java/quantumaidan/tillfix/Tillfix.java
+++ src/main/java/quantumaidan/tillfix/Tillfix.java
@@ -2,6 +2,9 @@ package quantumaidan.tillfix;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.TillableBlockRegistry;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.HoeItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +19,8 @@ public class Tillfix implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		TillableBlockRegistry.register(Blocks.PODZOL, HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.FARMLAND.getDefaultState()));
+		TillableBlockRegistry.register(Blocks.MYCELIUM, HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.FARMLAND.getDefaultState()));
+		LOGGER.info("TillFix Applied! - https://github.com/aidanfoss/tillFix");
 	}
 }
